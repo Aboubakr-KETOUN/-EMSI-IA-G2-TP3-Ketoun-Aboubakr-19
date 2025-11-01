0b4b780cf9ec658d48316af1e3afc4a3d883d28a
Aboubakr-KETOUN <aboubakrketoun@gmail.com>
2025-11-01 20:35:32 +0100

    changement de nom du config + test de nv path et parametre en lien

--- src/main/java/ma/emsi/ketoun/tp3/tp3_ketoun_aboubakr_19/HelloResource.java
+++ src/main/java/ma/emsi/ketoun/tp3/tp3_ketoun_aboubakr_19/HelloResource.java
@@ -2,13 +2,16 @@ package ma.emsi.ketoun.tp3.tp3_ketoun_aboubakr_19;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
-@Path("/hello-world")
+@Path("/hello/personnes")
 public class HelloResource {
+
     @GET
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Path("/{nom}")
+    public String hello(@PathParam("nom") String nom) {
+        return "Hello, " + nom;
     }
 }
