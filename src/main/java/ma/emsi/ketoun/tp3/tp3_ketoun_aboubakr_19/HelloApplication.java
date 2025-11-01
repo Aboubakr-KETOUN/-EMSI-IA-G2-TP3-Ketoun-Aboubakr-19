d0d3ef407edaca3d3e4983adb8699c80380002a1
Aboubakr-KETOUN <aboubakrketoun@gmail.com>
2025-11-01 20:16:11 +0100

    commit initiale

---
+++ src/main/java/ma/emsi/ketoun/tp3/tp3_ketoun_aboubakr_19/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.ketoun.tp3.tp3_ketoun_aboubakr_19;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/ma/emsi/ketoun/tp3/tp3_ketoun_aboubakr_19/HelloResource.java
@@ -0,0 +1,14 @@
+package ma.emsi.ketoun.tp3.tp3_ketoun_aboubakr_19;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
